03831ac71601d7809266c279e240aa3482c39664
pravatjana91 <97167349+pravatjana91@users.noreply.github.com>
2022-12-30 15:53:05 +0530

    service registry

--- td-micro-service/department-micro-service/src/main/java/com/td/departmentmicroservice/controller/DepartmentController.java
+++ td-micro-service/department-micro-service/src/main/java/com/td/departmentmicroservice/controller/DepartmentController.java
@@ -13,9 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/departments")
-//@Slf4j
+@Slf4j
 public class DepartmentController {
-    private static final Logger log = LoggerFactory.getLogger(DepartmentController.class);
     
     @Autowired
     private DepartmentService departmentService;
